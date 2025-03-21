0b480d091408165db4edf9e8a8a507e376f9bac8
Fred Perrier <frederique.perrier@gmail.com>
2025-03-21 08:46:51 +0100

    Start css coding

--- app/views/layout.php
+++ app/views/layout.php
@@ -13,19 +13,18 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" type="image/vnd.icon" href="/images/Group10.svg">
 </head>
 
 <body>
     <header class="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light headerNav">
-            <div class="border border-info col-6 d-flex justify-content-evenly align-items-center">
-                <img src="/images/logo.svg" alt="Logo Tom Troc">
+        <nav class="headerNav">
+            <div class="headerPublic">
+                <img src="/images/logo.svg" alt="Logo Tom Troc" class="logo">
                 <a href="index.php?action=home" class="link">Accueil</a>
                 <a href="index.php?action=books" class="link">Nos livres à l'échange</a>
             </div>
-            <div class="border border-info col-6 d-flex justify-content-evenly">
+            <div class="headerPrivate">
                 <span class="line">|</span>
                 <a href="index.php?action=messages" class="link">
                     <img src="/images/icon_messagerie.svg" alt="Messagerie">
--- app/views/templates/home.php
+++ app/views/templates/home.php
@@ -1,8 +1,7 @@
-<h2>Page d'accueil</h2>
 <section class="presentation">
     <div class="textPresentation">
-        <h3>Rejoignez nos lecteurs passionnés</h3>
-        <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
+        <p class="presentationTitle">Rejoignez nos lecteurs passionnés</p>
+        <p class="presentationContent">Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
         <button>Découvrir</button>
     </div>
     <div>
@@ -11,34 +10,51 @@
     </div>
 </section>
 <section class="lastBooks">
-    <h3>Les derniers livres ajoutés</h3>
+    <div class="lastBooksTitle">Les derniers livres ajoutés</div>
     <div>
         À venir : les quatre derniers livres enregistrés
     </div>
+    <button>Voir tous les livres</button>
 </section>
 <section class="operation">
-    <h3>Comment ça marche ?</h3>
+    <div class="operationTitle">Comment ça marche ?</div>
     <span>Échanger des livres avec TomTroc c'est simple et amusant ! Suivez ces étapes pour commencer :</span>
     <div class="operationStickers">
-        <div class="sticker">Inscrivez-vous gratuitement sur
-            notre plateforme</div>
-        <div class="sticker">Ajoutez les livres que vous souhaitez échanger à votre profil.</div>
-        <div class="sticker">Parcourez les livres disponibles chez d'autres membres.</div>
-        <div class="sticker">Proposez un échange et discutez avec d'autres passionnés de lecture.</div>
+        <div class="sticker">
+            <p class="stickerText">
+                Inscrivez-vous gratuitement sur
+                notre plateforme
+            </p>
+        </div>
+        <div class="sticker">
+            <p class="stickerText">
+                Ajoutez les livres que vous souhaitez échanger à votre profil.
+            </p>
+        </div>
+        <div class="sticker">
+            <p class="stickerText">
+                Parcourez les livres disponibles chez d'autres membres.
+            </p>
+        </div>
+        <div class="sticker">
+            <p class="stickerText">
+                Proposez un échange et discutez avec d'autres passionnés de lecture.
+            </p>
+        </div>
     </div>
     <button>Voir tous les livres</button>
 </section>
 <section class="values">
     <img src="/images/mask_group.png" alt="" class="pictureValues">
     <div class="textValues">
-        <h3 class="titleValues">Nos valeurs</h3>
+        <div class="titleValues">Nos valeurs</div>
         <p class="tomValues">
             Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes. <br>
 
             Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. <br>
 
             Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
-        <p>
+        <p class="sign">
             <span>L'équipe Tom Troc</span>
             <img src="/images/vector2.svg" alt="">
         </p>
