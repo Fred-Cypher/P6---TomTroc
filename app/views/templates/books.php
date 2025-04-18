7aae6c683fe58ffdb2efa23add6ff6d8af6c3258
Fred Perrier <frederique.perrier@gmail.com>
2025-04-18 15:02:32 +0200

    Add css

--- app/views/templates/books.php
+++ app/views/templates/books.php
@@ -1,16 +1,17 @@
-<section>
-    <h2>Nos livres à l'échange</h2>
-    <form action="">
-        <input type="search" placeholder="Rechercher un livre">
+<section class="exchangeHeader">
+    <h2 class="exchangeTitle">Nos livres à l'échange</h2>
+    <form action="" class="exchangeSearch"> 
+        <input type="search" placeholder="Rechercher un livre" class="inputSearch">
+        <img src="/images/loupe.svg" alt="" class="loupe">
     </form>
 </section>
 <section class="exchangeBooks">
     
     <?php
 
-use App\services\Utils;
+    use App\services\Utils;
 
-foreach ($books as $book): ?>
+    foreach ($books as $book): ?>
         <article class="bookCard">
             <a href="index.php?action=detailBook&id=<?= $book->getId() ?>" class="cardLink">
                 <p class="itemImage">
@@ -20,9 +21,9 @@ foreach ($books as $book): ?>
                     <img src="/uploads/covers/<?= htmlspecialchars($book->getCover()) ?>" alt="" class="mediumCover">
                 </p>
                 <div class="bookInformation">
-                    <p class="exchangeBookTitle"><?= Utils::format($book->getTitle()) ?></p>
-                    <p class="exchangeBookAuthor"><?= Utils::format($book->getAuthor()) ?></p>
-                    <p class="exchangeBookSeller">Vendu par : <?= Utils::format($book->getUserPseudo()) ?></p>
+                    <p class="exchangeBookTitle"><?= htmlspecialchars($book->getTitle()) ?></p>
+                    <p class="exchangeBookAuthor"><?= htmlspecialchars($book->getAuthor()) ?></p>
+                    <p class="exchangeBookSeller">Vendu par : <?= htmlspecialchars($book->getUserPseudo()) ?></p>
                 </div>
             </a>
         </article>
--- app/views/templates/login.php
+++ app/views/templates/login.php
@@ -1,15 +1,22 @@
-<h2>Connexion</h2>
+<section class="authentication">
+    <div class="authenticationText">
+        <h2 class="authenticationTitle">Connexion</h2>
 
-<?php if (!empty($message)): ?>
-    <p><?= $message ?></p>
-<?php endif; ?>
+        <?php if (!empty($message)): ?>
+            <p><?= $message ?></p>
+        <?php endif; ?>
 
-<form action="index.php?action=login" method="POST">
-    <label for="email">Adresse email</label>
-    <input type="text" name="email" id="email" required>
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" id="password" required>
-    <button class="submit">Se connecter</button>
-</form>
+        <form action="index.php?action=login" method="POST" class="authenticationForm">
+            <label for="email" class="authenticationLabel">Adresse email</label>
+            <input type="text" name="email" id="email" class="authenticationInput" required>
+            <label for="password" class="authenticationLabel">Mot de passe</label>
+            <input type="password" name="password" id="password" class="authenticationInput" required>
+            <button class="submit">Se connecter</button>
+        </form>
 
-<p>Pas de compte ? <a href="index.php?action=register">Inscrivez-vous</a></p>
+        <p class="authenticationQuestion">Pas de compte ? <a href="index.php?action=register">Inscrivez-vous</a></p>
+    </div>
+    <div class="authenticationIllustration">
+        <img src="/images/mask_group-1.png" class="authenticationPicture">
+    </div>
+</section>
